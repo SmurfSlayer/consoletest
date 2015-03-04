7283bb41e122af7a6c8fa641c8a7434249a58181
fhunter <fhunter@wsu.edu>
2015-03-03 16:21:10 -0800

    testconsoleapp

---
+++ ConsoleTest1/ConsoleTest1.cpp
@@ -0,0 +1,12 @@
+// ConsoleTest1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("test");
+	return 0;
+}
+
